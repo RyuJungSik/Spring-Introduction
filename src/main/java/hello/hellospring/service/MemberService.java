f475d41cee2651648b7b9a203880e1f5c5c69dbe
unknown <xvbc123@gmail.com>
2022-11-14 03:26:57 +0900

    Add : lesson23~25

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -20,10 +20,22 @@ public class MemberService {
 
     //회원가입
     public Long join(Member member){
-        //같은 이름의 중복회원 X
-        validateDuplicateMember(member);//중복회원 검증
-        memberRepository.save(member);
-        return member.getId();
+
+        long start = System.currentTimeMillis();
+        try{
+            //같은 이름의 중복회원 X
+            validateDuplicateMember(member);//중복회원 검증
+            memberRepository.save(member);
+            return member.getId();
+        }finally {
+            long finish = System.currentTimeMillis();
+            long timeMs=finish-start;
+            System.out.println("join = " + timeMs + "ms");
+
+        }
+
+
+
 
 
     }
