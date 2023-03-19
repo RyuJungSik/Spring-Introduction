92ff6b29b59e6ef4b3da7633b7396d1117970c25
RyuJungSik <xvbc123@gmail.com>
2023-03-20 01:22:52 +0900

    Add : 스프링 데이터 JPA 추가

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -12,12 +12,20 @@ import javax.sql.DataSource;
 @Configuration
 public class SpringConfig {
 
-private EntityManager em;
+private final MemberRepository memberRepository;
 
 @Autowired
-public SpringConfig(EntityManager em) {
-    this.em = em;
+public SpringConfig(MemberRepository memberRepository) {
+    this.memberRepository = memberRepository;
 }
+
+//private EntityManager em;
+//
+//@Autowired
+//public SpringConfig(EntityManager em) {
+//    this.em = em;
+//}
+
 //private DataSource dataSource;
 //@Autowired
 //public SpringConfig(DataSource dataSource) {
@@ -26,15 +34,15 @@ public SpringConfig(EntityManager em) {
 
 @Bean
 public MemberService memberService(){
-    return new MemberService(memberRepository());
+    return new MemberService(memberRepository);
 }
 
-@Bean
-public MemberRepository memberRepository(){
+//@Bean
+//public MemberRepository memberRepository(){
 //    return new JdbcTemplateMemberRepository(dataSource);
 //    return new JdbcMemberRepository(dataSource);
 //    return new MemoryMemberRepository();
-    return new JpaMemberRepository(em);
-}
+//    return new JpaMemberRepository(em);
+//}
 
 }
